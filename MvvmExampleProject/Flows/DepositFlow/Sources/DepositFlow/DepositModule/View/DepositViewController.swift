05217d0cde3e15c574bb096270a5eac9d1ea8803
Ilya Knyazkov <fivecoil@gmail.com>
2022-05-02 16:06:00 +0300

    Layout DepositSumCell on DepositViewController

--- MvvmExampleProject/Flows/DepositFlow/Sources/DepositFlow/DepositModule/View/DepositViewController.swift
+++ MvvmExampleProject/Flows/DepositFlow/Sources/DepositFlow/DepositModule/View/DepositViewController.swift
@@ -3,19 +3,24 @@
 //
 
 import UIKit
-import Core
+import Library
+import Combine
 
 final class DepositViewController<ViewModel: DepositViewModel>: UIViewController {
 
     // MARK: - Private Properties
 
     private var viewModel: ViewModel?
+    private let tableView = UITableView()
+    private lazy var adapter = tableView.rddm.baseBuilder.build()
+    private var cancellableEventsContainer: Set<AnyCancellable> = []
 
     // MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupInitialState()
+        fillAdapter()
     }
 
     // MARK: - Internal Methods
@@ -26,10 +31,63 @@ final class DepositViewController<ViewModel: DepositViewModel>: UIViewController
 
 }
 
+// MARK: - Generators
+
+private extension DepositViewController {
+
+    func fillAdapter() {
+        let generators: [TableCellGenerator] = [
+            getVerticalSpaceGenerator(height: 14),
+            getDepositSumCellGenerator()
+        ]
+        adapter.addCellGenerators(generators)
+    }
+
+    func getDepositSumCellGenerator() -> TableCellGenerator {
+        let model = DepositSumCell.Model(
+            title: "Сумма депозита",
+            state: .common(text: "От 15 000 ₽")
+        )
+        model.sumDidChangePublisher
+            .sink { _ in }
+            .store(in: &cancellableEventsContainer)
+        let depositSumCellGenerator = BaseNonReusableCellGenerator<DepositSumCell>(with: model)
+        return depositSumCellGenerator
+    }
+
+    func getVerticalSpaceGenerator(height: CGFloat) -> TableCellGenerator {
+        let verticalSpaceCellGenerator = BaseNonReusableCellGenerator<VerticalSpaceCell>(with: .init(height: height))
+        return verticalSpaceCellGenerator
+    }
+
+}
+
 // MARK: - Private Methods
 
 private extension DepositViewController {
 
-    func setupInitialState() { }
+    func setupInitialState() {
+        view.backgroundColor = .white
+
+        configureTableView()
+        configureNavigationBar()
+    }
+
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .none
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+    }
+
+    func configureNavigationBar() {
+        title = "Вклад"
+    }
 
 }
